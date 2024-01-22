2b2dcc7197f2773d8e3f79acef9a2e12862f1e12
Arthur Saito <arthur_saito1998@hotmail.com>
2024-01-22 19:43:50 -0300

    feat: implementing defaultCreateCategoryUseCase

---
+++ application/src/main/java/com/pixelflicks/admin/catalogo/application/category/create/CreateCategoryUseCase.java
@@ -0,0 +1,7 @@
+package com.pixelflicks.admin.catalogo.application.category.create;
+
+import com.pixelflicks.admin.catalogo.application.UseCase;
+
+public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+
+}
