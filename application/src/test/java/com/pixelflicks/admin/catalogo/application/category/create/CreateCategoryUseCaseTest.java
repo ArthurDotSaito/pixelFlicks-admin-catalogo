b5b051f797a11b6f3f1a82fe0d6c313d52d52baa
Arthur Saito <arthur_saito1998@hotmail.com>
2024-01-22 20:15:09 -0300

    test: create a invalidNameInCategory test

--- application/src/test/java/com/pixelflicks/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
+++ application/src/test/java/com/pixelflicks/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
@@ -3,14 +3,23 @@ package com.pixelflicks.admin.catalogo.application.category.create;
 import com.pixelflicks.admin.catalogo.domain.category.CategoryGateway;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Objects;
 
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 
+@ExtendWith(MockitoExtension.class)
 public class CreateCategoryUseCaseTest {
 
+    @InjectMocks
+    private DefaultCreateCategoryUseCase useCase;
+    @Mock
+    private CategoryGateway categoryGateway;
     @Test
     public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId(){
         final var expectedName = "Filmes";
@@ -20,11 +29,7 @@ public class CreateCategoryUseCaseTest {
 
         final var aCommand = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
 
-        final CategoryGateway categoryGateway = Mockito.mock(CategoryGateway.class);
-
         Mockito.when(categoryGateway.create(Mockito.any())).thenAnswer(returnsFirstArg());
-
-        final var useCase = new DefaultCreateCategoryUseCase(categoryGateway);
 
         final var actualOutput = useCase.execute(aCommand);
 
@@ -41,5 +46,18 @@ public class CreateCategoryUseCaseTest {
                             && Objects.isNull(aCategory.getDeletedAt());
                 }));
 
+    }
+
+    public void givenAInvalidName_whenCallsCreateCategory_thenShouldReturnAnDomainException(){
+        final var expectedName = "  ";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedCategory = true;
+
+        final var aCommand = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        Mockito.when(categoryGateway.create(Mockito.any())).thenAnswer(returnsFirstArg());
+
+        final var actualOutput = useCase.execute(aCommand);
     }
 }
