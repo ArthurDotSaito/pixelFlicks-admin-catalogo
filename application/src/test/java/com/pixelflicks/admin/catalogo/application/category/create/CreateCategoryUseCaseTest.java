93324c2146a15023a5dbef8401405b562b40fcce
Arthur Saito <arthur_saito1998@hotmail.com>
2024-01-22 23:16:54 -0300

    test: adding a test to check random Gateway exceptions

--- application/src/test/java/com/pixelflicks/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
+++ application/src/test/java/com/pixelflicks/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
@@ -75,7 +75,6 @@ public class CreateCategoryUseCaseTest {
 
         final var actualOutput = useCase.execute(aCommand);
 
-
         Assertions.assertNotNull(actualOutput);
         Assertions.assertNotNull(actualOutput.id());
         Mockito.verify(categoryGateway, times(1)).
@@ -87,6 +86,32 @@ public class CreateCategoryUseCaseTest {
                             && Objects.nonNull(aCategory.getCreatedAt())
                             && Objects.nonNull(aCategory.getUpdatedAt())
                             && Objects.nonNull(aCategory.getDeletedAt());
+                }));
+    }
+
+    @Test
+    public void givenAValidCommand_whenGatewayThrowsAnException_thenShouldReturnAnException(){
+        final String expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "Gateway Error";
+
+        final var aCommand =  CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+        Mockito.when(categoryGateway.create(Mockito.any())).thenThrow(new IllegalStateException(expectedErrorMessage));
+
+        final var actualException = Assertions.assertThrows(IllegalStateException.class, () -> useCase.execute(aCommand));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
+
+        Mockito.verify(categoryGateway, times(1)).
+                create(Mockito.argThat(aCategory ->{
+                    return Objects.equals(expectedName, aCategory.getName())
+                            && Objects.equals(expectedDescription, aCategory.getDescription())
+                            && Objects.equals(expectedIsActive, aCategory.isActive())
+                            && Objects.nonNull(aCategory.getId())
+                            && Objects.nonNull(aCategory.getCreatedAt())
+                            && Objects.nonNull(aCategory.getUpdatedAt())
+                            && Objects.isNull(aCategory.getDeletedAt());
                 }));
     }
 }
