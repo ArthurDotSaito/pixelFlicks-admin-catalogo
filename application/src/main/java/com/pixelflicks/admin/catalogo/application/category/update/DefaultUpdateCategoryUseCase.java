f4ecb8473d04db4e8d2a808a81cdfceff2251d81
Arthur Saito <arthur_saito1998@hotmail.com>
2024-01-24 14:35:12 -0300

    [feat] add updateCategory default useCase

--- application/src/main/java/com/pixelflicks/admin/catalogo/application/category/update/DefaultUpdateCategoryUseCase.java
+++ application/src/main/java/com/pixelflicks/admin/catalogo/application/category/update/DefaultUpdateCategoryUseCase.java
@@ -1,10 +1,12 @@
 package com.pixelflicks.admin.catalogo.application.category.update;
 
+import com.pixelflicks.admin.catalogo.domain.category.Category;
 import com.pixelflicks.admin.catalogo.domain.category.CategoryGateway;
 import com.pixelflicks.admin.catalogo.domain.category.CategoryID;
 import com.pixelflicks.admin.catalogo.domain.exceptions.DomainException;
 import com.pixelflicks.admin.catalogo.domain.validation.Error;
 import com.pixelflicks.admin.catalogo.domain.validation.handler.Notification;
+import io.vavr.API;
 import io.vavr.control.Either;
 
 import java.util.function.Supplier;
@@ -30,7 +32,12 @@ public class DefaultUpdateCategoryUseCase extends UpdateCategoryUseCase{
         final var notification = Notification.create();
 
         aCategory.update(aName,aDescription,isActive).validate(notification);
-        return notification.hasErrors() ? Either.Left(notification) : update(Category aCategory);
+        return notification.hasErrors() ? API.Left(notification) : update(aCategory);
+    }
+
+    private Either<Notification, UpdateCategoryOutput> update(final Category aCategory){
+        return API.Try(() ->this.categoryGateway.update(aCategory)).toEither()
+                .bimap(Notification::create, UpdateCategoryOutput::from);
     }
 
     private static Supplier<DomainException> notFound(CategoryID anId) {
