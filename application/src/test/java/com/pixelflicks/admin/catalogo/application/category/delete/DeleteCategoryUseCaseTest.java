bc9dc92c91556f92c84e912e55344c846bd3f955
Arthur Saito <arthur_saito1998@hotmail.com>
2024-01-27 14:28:42 -0300

    [test] add three test cases in deleteCategory

--- application/src/test/java/com/pixelflicks/admin/catalogo/application/category/delete/DeleteCategoryUseCaseTest.java
+++ application/src/test/java/com/pixelflicks/admin/catalogo/application/category/delete/DeleteCategoryUseCaseTest.java
@@ -2,6 +2,7 @@ package com.pixelflicks.admin.catalogo.application.category.delete;
 
 import com.pixelflicks.admin.catalogo.domain.category.CategoryGateway;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -22,5 +23,19 @@ public class DeleteCategoryUseCaseTest {
         Mockito.reset(categoryGateway);
     }
 
+    @Test
+    public void givenAValidId_whenCallsDeleteCategory_shouldBeOk(){
+
+    }
+
+    @Test
+    public void givenAInvalidId_whenCallsDeleteCategory_shouldBeOk(){
+
+    }
+
+    @Test
+    public void givenAValidId_whenGatewayThrowsError_shouldReturnOk(){
+
+    }
 
 }
