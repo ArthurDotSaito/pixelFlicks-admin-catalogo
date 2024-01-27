c8e9c05b8d41b1b4f51f3561ed6d4367e81e83a4
Arthur Saito <arthur_saito1998@hotmail.com>
2024-01-27 14:25:16 -0300

    [chore] add DeleteCategoryTest

---
+++ application/src/test/java/com/pixelflicks/admin/catalogo/application/category/delete/DeleteCategoryUseCaseTest.java
@@ -0,0 +1,26 @@
+package com.pixelflicks.admin.catalogo.application.category.delete;
+
+import com.pixelflicks.admin.catalogo.domain.category.CategoryGateway;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class DeleteCategoryUseCaseTest {
+
+    @InjectMocks
+    private DefaultDeleteCategoryUseCase useCase;
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
+    @BeforeEach
+    void cleanUp(){
+        Mockito.reset(categoryGateway);
+    }
+
+
+}
