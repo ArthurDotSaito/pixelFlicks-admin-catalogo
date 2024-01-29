3c6901cd4b982ab89a76f1e7dc0a65e53a144027
Arthur Saito <arthur_saito1998@hotmail.com>
2024-01-29 18:20:27 -0300

    [chore] adding getCategoryByIdUseCaseTest

---
+++ application/src/test/java/com/pixelflicks/admin/catalogo/application/category/retrieve/GetCategoryByIdUseCaseTest.java
@@ -0,0 +1,26 @@
+package com.pixelflicks.admin.catalogo.application.category.retrieve;
+
+import com.pixelflicks.admin.catalogo.domain.category.CategoryGateway;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class GetCategoryByIdUseCaseTest {
+
+    @InjectMocks
+    private DefaultGetCategoryByIdUseCase useCase;
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
+    @BeforeEach
+    void cleanUp(){
+        Mockito.reset(categoryGateway);
+    }
+
+
+}
