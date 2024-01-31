a11d7bfcd8259664ecf7f9b49fe13fa6b78e36dc
Arthur Saito <arthur_saito1998@hotmail.com>
2024-01-31 20:10:54 -0300

    [chore] add SpringBoot on infrastructure layer

---
+++ infrastructure/src/main/java/com/pixelflicks/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.pixelflicks.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.pixelflicks.admin.catalogo")
+public class WebServerConfig {
+}
