f22d41218c7fab0838504b26afc32f0724b4a855
Arthur Saito <arthur_saito1998@hotmail.com>
2024-02-05 14:07:03 -0300

    [test] add create test at categoryGateway

--- infrastructure/src/test/java/com/pixelflicks/admin/catalogo/infrastructure/category/CategoryMySQLGatewayTest.java
+++ infrastructure/src/test/java/com/pixelflicks/admin/catalogo/infrastructure/category/CategoryMySQLGatewayTest.java
@@ -1,5 +1,6 @@
 package com.pixelflicks.admin.catalogo.infrastructure.category;
 
+import com.pixelflicks.admin.catalogo.domain.category.Category;
 import com.pixelflicks.admin.catalogo.infrastructure.MySQLGatewayTest;
 import com.pixelflicks.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
 import org.junit.jupiter.api.Assertions;
@@ -19,5 +20,27 @@ public class CategoryMySQLGatewayTest {
     public void testInjectionDependencies(){
         Assertions.assertNotNull(categoryGateway);
         Assertions.assertNotNull(categoryRepository);
+    }
+    @Test
+    public void givenAValidCategory_whenCallsCreate_thenShouldReturnANewCategory(){
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var aCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        final var actualCategory = categoryGateway.create(aCategory);
+
+        Assertions.assertEquals(1, categoryRepository.count());
+        Assertions.assertEquals(aCategory.getId(), actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertEquals(aCategory.getCreatedAt(), actualCategory.getCreatedAt());
+        Assertions.assertEquals(aCategory.getUpdatedAt(), actualCategory.getUpdatedAt());
+        Assertions.assertEquals(aCategory.getDeletedAt(), actualCategory.getDeletedAt());
+        Assertions.assertNull( actualCategory.getDeletedAt());
     }
 }
