19e28d71e58a88f3f78a3ce88a56ed77bc871d66
Arthur Saito <arthur_saito1998@hotmail.com>
2024-02-09 20:58:46 -0300

    nothing

--- infrastructure/src/main/java/com/pixelflicks/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/pixelflicks/admin/catalogo/infrastructure/Main.java
@@ -1,24 +1,9 @@
 package com.pixelflicks.admin.catalogo.infrastructure;
 
-import com.pixelflicks.admin.catalogo.application.category.create.CreateCategoryUseCase;
-import com.pixelflicks.admin.catalogo.application.category.delete.DeleteCategoryUseCase;
-import com.pixelflicks.admin.catalogo.application.category.retrieve.get.GetCategoryByIdUseCase;
-import com.pixelflicks.admin.catalogo.application.category.retrieve.list.ListCategoriesUseCase;
-import com.pixelflicks.admin.catalogo.application.category.update.UpdateCategoryUseCase;
-import com.pixelflicks.admin.catalogo.domain.category.Category;
-import com.pixelflicks.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
-import com.pixelflicks.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
 import com.pixelflicks.admin.catalogo.infrastructure.configuration.WebServerConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.env.AbstractEnvironment;
-
-import java.util.List;
 
 @SpringBootApplication
 public class Main {
@@ -26,19 +11,5 @@ public class Main {
         System.out.println("Hello world!");
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
-    }
-
-    @Bean
-    @DependsOnDatabaseInitialization
-    ApplicationRunner runner(
-            @Autowired CreateCategoryUseCase createCategoryUseCase,
-            @Autowired UpdateCategoryUseCase updateCategoryUseCase,
-            @Autowired DeleteCategoryUseCase deleteCategoryUseCase,
-            @Autowired ListCategoriesUseCase listCategoriesUseCase,
-            @Autowired GetCategoryByIdUseCase getCategoryByIdUseCase
-            ){
-        return args -> {
-
-        };
     }
 }
