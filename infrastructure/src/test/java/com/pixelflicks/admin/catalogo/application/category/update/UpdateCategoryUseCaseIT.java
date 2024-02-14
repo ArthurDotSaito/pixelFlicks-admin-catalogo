3f0649d39592c4bb9d21a5767004f7e77e1944bc
Arthur Saito <arthur_saito1998@hotmail.com>
2024-02-14 19:11:33 -0300

    [test] add test which return a correct Id if everything is ok

---
+++ infrastructure/src/test/java/com/pixelflicks/admin/catalogo/application/category/update/UpdateCategoryUseCaseIT.java
@@ -0,0 +1,70 @@
+package com.pixelflicks.admin.catalogo.application.category.update;
+
+import com.pixelflicks.admin.catalogo.IntegrationTest;
+import com.pixelflicks.admin.catalogo.domain.category.Category;
+import com.pixelflicks.admin.catalogo.domain.category.CategoryGateway;
+import com.pixelflicks.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
+import com.pixelflicks.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.Mockito.times;
+
+@IntegrationTest
+public class UpdateCategoryUseCaseIT {
+
+    @Autowired
+    private UpdateCategoryUseCase useCase;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @SpyBean
+    private CategoryGateway categoryGateway;
+
+    @Test
+    public void givenAValidCommand_whenCallsUpdateCategory_shouldReturnCategoryId(){
+        final var aCategory = Category.newCategory("Film", null, true);
+
+        save(aCategory);
+
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedId = aCategory.getId();
+
+        final var aCommand = UpdateCategoryCommand.with(expectedId.getValue(), expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertEquals(1, categoryRepository.count());
+
+        final var actualOutput = useCase.execute(aCommand).get();
+
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.id());
+
+        final var actualCategory = categoryRepository.findById(expectedId.getValue()).get();
+
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertEquals(aCategory.getCreatedAt(), actualCategory.getCreatedAt());
+        Assertions.assertTrue(aCategory.getCreatedAt().isBefore(actualCategory.getUpdatedAt()));
+        Assertions.assertNull(actualCategory.getDeletedAt());
+    }
+
+    private void  save(final Category... aCategory){
+        categoryRepository.saveAllAndFlush(
+                Arrays.stream(aCategory)
+                        .map(CategoryJpaEntity::from)
+                        .toList()
+        );
+    }
+}
