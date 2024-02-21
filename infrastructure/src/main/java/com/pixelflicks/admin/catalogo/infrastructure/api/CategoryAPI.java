a1d80b4f56a75631dd2e521cb3bb2e595f2532c0
Arthur Saito <arthur_saito1998@hotmail.com>
2024-02-21 19:55:26 -0300

    [feat] add openAPI swagger

--- infrastructure/src/main/java/com/pixelflicks/admin/catalogo/infrastructure/api/CategoryAPI.java
+++ infrastructure/src/main/java/com/pixelflicks/admin/catalogo/infrastructure/api/CategoryAPI.java
@@ -1,6 +1,10 @@
 package com.pixelflicks.admin.catalogo.infrastructure.api;
 
 import com.pixelflicks.admin.catalogo.domain.pagination.Pagination;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +13,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping(value = "categories")
+@Tag(name = "Categories")
 public interface CategoryAPI {
 
     @PostMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
+
+    @Operation(summary = "Create a new category")
+    @ApiResponses(value ={
+            @ApiResponse(responseCode = "201", description = "Created successfully"),
+            @ApiResponse(responseCode = "422", description = "Validation Error"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    }
+    )
     ResponseEntity<?> createCategory();
 
+
     @GetMapping
+    @Operation(summary = "List all categories paginated")
+    @ApiResponses(value={
+            @ApiResponse(responseCode = "200", description = "Listed successfully"),
+            @ApiResponse(responseCode = "422", description = "Invalid parameter was received"),
+            @ApiResponse(responseCode = "422", description = "Internal server error"),
+    })
     Pagination<?> listCategories(
             @RequestParam(name = "search", required = false, defaultValue = "") final String search,
             @RequestParam(name = "page", required = false, defaultValue = "0") final int page,
