04a67d9b243d57501435699aa1799b0959d45f8d
Arthur Saito <arthur_saito1998@hotmail.com>
2024-02-21 22:24:05 -0300

    [test] implementing createCategory controller test

--- infrastructure/src/test/java/com/pixelflicks/admin/catalogo/infrastructure/api/CategoryApiTest.java
+++ infrastructure/src/test/java/com/pixelflicks/admin/catalogo/infrastructure/api/CategoryApiTest.java
@@ -1,11 +1,28 @@
 package com.pixelflicks.admin.catalogo.infrastructure.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pixelflicks.admin.catalogo.ControllerTest;
+import com.pixelflicks.admin.catalogo.application.category.create.CreateCategoryCommand;
+import com.pixelflicks.admin.catalogo.application.category.create.CreateCategoryOutput;
 import com.pixelflicks.admin.catalogo.application.category.create.CreateCategoryUseCase;
+import com.pixelflicks.admin.catalogo.application.category.retrieve.get.CategoryOutput;
+import com.pixelflicks.admin.catalogo.domain.category.CategoryID;
+import com.pixelflicks.admin.catalogo.infrastructure.category.models.CreateCategoryApiInput;
+import io.vavr.API;
+import jdk.jfr.ContentType;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Objects;
 
 @ControllerTest(controllers = CategoryAPI.class)
 public class CategoryApiTest {
@@ -13,7 +30,36 @@ public class CategoryApiTest {
     @Autowired
     private MockMvc mvc;
 
+    private ObjectMapper mapper;
+
     @MockBean
     private CreateCategoryUseCase createCategoryUseCase;
+
+    @Test
+    public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() throws Exception{
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var aInput = new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+        Mockito.when(createCategoryUseCase.execute(Mockito.any()))
+                .thenReturn(API.Right(CreateCategoryOutput.from(CategoryID.from("123"))));
+
+        final var request = MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(aInput));
+
+        this.mvc.perform(request).andDo(MockMvcResultHandlers.log()).andExpectAll(
+                MockMvcResultMatchers.status().isCreated(),
+                MockMvcResultMatchers.header().string("Location", "/categories/123")
+        );
+
+        Mockito.verify(createCategoryUseCase, Mockito.times(1)).execute(Mockito.argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+        ));
+    }
 
 }
