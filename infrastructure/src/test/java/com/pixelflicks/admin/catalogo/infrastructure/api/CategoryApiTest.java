ec320c4f6da01a91b4822b06344003f0c43c390e
Arthur Saito <arthur_saito1998@hotmail.com>
2024-03-05 19:32:30 -0300

    [test] add a test which should return a notification

--- infrastructure/src/test/java/com/pixelflicks/admin/catalogo/infrastructure/api/CategoryApiTest.java
+++ infrastructure/src/test/java/com/pixelflicks/admin/catalogo/infrastructure/api/CategoryApiTest.java
@@ -238,6 +238,43 @@ public class CategoryApiTest {
     }
 
     @Test
+    public void givenAInvalidName_whenCallsUpdateCategory_thenShouldReturnANotification() throws Exception{
+        //given
+        final var expectedId = "123";
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedErrorCount = 1;
+
+        when(updateCategoryUseCase.execute(any()))
+                .thenReturn(API.Left(Notification.create(new Error(expectedErrorMessage))));
+
+        final var aCommand = new UpdateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+        //when
+        final var request = put("/categories/{id}", expectedId)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(aCommand));
+
+        final var response = this.mvc.perform(request).andDo(log());
+
+        //then
+        response.andExpect(status().isUnprocessableEntity())
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.errors", hasSize(1)))
+                .andExpect(jsonPath("$.errors[0].message", equalTo(expectedErrorMessage)));
+
+        verify(updateCategoryUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+        ));
+    }
+
+    @Test
     public void givenAInvalidName_whenCallsUpdateCategory_thenShouldReturnAnDomainException() throws Exception{
         //given
         final var expectedId = "not-found";
