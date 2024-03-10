56f96d9a9c255793078dd77ab515383942699e2f
Arthur Saito <arthur_saito1998@hotmail.com>
2024-03-10 15:25:47 -0300

    [test] add a unmarshalling test

--- infrastructure/src/test/java/com/pixelflicks/admin/catalogo/infrastructure/category/models/CategoryResponseTest.java
+++ infrastructure/src/test/java/com/pixelflicks/admin/catalogo/infrastructure/category/models/CategoryResponseTest.java
@@ -1,10 +1,45 @@
 package com.pixelflicks.admin.catalogo.infrastructure.category.models;
 
 import com.pixelflicks.admin.catalogo.JacksonTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.json.JacksonTester;
+
+import java.time.Instant;
 
 @JacksonTest
 public class CategoryResponseTest {
     private JacksonTester<CategoryRespose> json;
+
+    @Test
+    public void testMarshall() throws Exception{
+        final var expectId = "12345";
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = false;
+        final var expectedCreatedAt = Instant.now();
+        final var expectedUpdatedAt = Instant.now();
+        final var expectedDeletedAt = Instant.now();
+
+        final var response = new CategoryRespose(
+                expectId,
+                expectedName,
+                expectedDescription,
+                expectedIsActive,
+                expectedCreatedAt,
+                expectedUpdatedAt,
+                expectedDeletedAt
+        );
+
+        final var actualJson = this.json.write(response);
+
+        Assertions.assertThat(actualJson)
+                .hasJsonPathValue("$.id", expectId)
+                .hasJsonPathValue("$.name", expectedDescription)
+                .hasJsonPathValue("$.is_description", expectedDescription)
+                .hasJsonPathValue("$.is_createdAt", expectedCreatedAt)
+                .hasJsonPathValue("$.is_deletedAt", expectedDeletedAt)
+                .hasJsonPathValue("$.is_updatedAt", expectedUpdatedAt);
+    }
 
 }
