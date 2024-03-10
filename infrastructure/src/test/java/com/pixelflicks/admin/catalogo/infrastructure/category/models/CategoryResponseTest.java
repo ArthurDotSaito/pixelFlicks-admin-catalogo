f4d06d4cd6eb29958f219132dcb2a8a90a723110
Arthur Saito <arthur_saito1998@hotmail.com>
2024-03-10 14:22:30 -0300

    [chore] add json serialization test

---
+++ infrastructure/src/test/java/com/pixelflicks/admin/catalogo/infrastructure/category/models/CategoryResponseTest.java
@@ -0,0 +1,10 @@
+package com.pixelflicks.admin.catalogo.infrastructure.category.models;
+
+import com.pixelflicks.admin.catalogo.JacksonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+@JacksonTest
+public class CategoryResponseTest {
+    private JacksonTester<CategoryRespose> json;
+
+}
