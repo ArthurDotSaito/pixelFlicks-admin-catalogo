862a55d74b0f55a200a9f2f2dceab25a0acd098a
Arthur Saito <arthur_saito1998@hotmail.com>
2024-03-12 23:00:54 -0300

    [chore] config database e2e test container

--- infrastructure/src/test/java/com/pixelflicks/admin/catalogo/e2e/CategoryE2ETest.java
+++ infrastructure/src/test/java/com/pixelflicks/admin/catalogo/e2e/CategoryE2ETest.java
@@ -1,6 +1,8 @@
 package com.pixelflicks.admin.catalogo.e2e;
 
 import com.pixelflicks.admin.catalogo.E2ETest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
@@ -13,15 +15,20 @@ public class CategoryE2ETest {
 
     @Container
     //TODO: Change latest to version
-    private static final MySQLContainer MY_SQL_CONTAINER = new MySQLContainer("mysql:latest")
+    private static final MySQLContainer MYSQL_CONTAINER = new MySQLContainer("mysql:latest")
             .withUsername("root")
             .withPassword("12345")
             .withDatabaseName("adm_videos");
 
     @DynamicPropertySource
     public static void setDatasourceProperties(final DynamicPropertyRegistry registry){
-        final var mappedPort = MY_SQL_CONTAINER.getMappedPort(3306);
+        final var mappedPort = MYSQL_CONTAINER.getMappedPort(3306);
         System.out.printf("Container is running on port %s\n", mappedPort);
         registry.add("mysql.port", () -> mappedPort);
+    }
+
+    @Test
+    public void testWorking(){
+        Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
     }
 }
