2a8d2882666af39b49daa6070b1205c4309186a8
Arthur Saito <arthur_saito1998@hotmail.com>
2024-03-12 21:38:05 -0300

    [chore] removing unused test and refactor extension

--- infrastructure/src/test/java/com/pixelflicks/admin/catalogo/MySQLGatewayTest.java
+++ infrastructure/src/test/java/com/pixelflicks/admin/catalogo/MySQLGatewayTest.java
@@ -1,17 +1,12 @@
 package com.pixelflicks.admin.catalogo;
 
-import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.lang.annotation.*;
-import java.util.Collection;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
@@ -21,7 +16,7 @@ import java.util.Collection;
         @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".[MySQLGateway]")
 })
 @DataJpaTest
-@ExtendWith(CleanUpExtension.class)
+@ExtendWith(MySQLCleanUpExtension.class)
 public @interface MySQLGatewayTest {
 
 }
