7a2efa37b7a19fb12beae73f1ed1d579cd1b473b
Arthur Saito <arthur_saito1998@hotmail.com>
2024-04-01 20:36:20 -0300

    add helper method givenACategory

--- infrastructure/src/test/java/com/pixelflicks/admin/catalogo/e2e/categories/CategoryE2ETest.java
+++ infrastructure/src/test/java/com/pixelflicks/admin/catalogo/e2e/categories/CategoryE2ETest.java
@@ -1,13 +1,23 @@
 package com.pixelflicks.admin.catalogo.e2e.categories;
 
 import com.pixelflicks.admin.catalogo.E2ETest;
+import com.pixelflicks.admin.catalogo.domain.category.CategoryID;
+import com.pixelflicks.admin.catalogo.infrastructure.category.models.CreateCategoryRequest;
+import com.pixelflicks.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import com.pixelflicks.admin.catalogo.infrastructure.configuration.json.Json;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @E2ETest
 @Testcontainers
@@ -27,8 +37,35 @@ public class CategoryE2ETest {
         registry.add("mysql.port", () -> mappedPort);
     }
 
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Test
-    public void testWorking(){
+    public void asACatelogAdminIShouldBeAbleToCreateANewCategoryWithValidValues(){
         Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+    }
+
+    private CategoryID givenACategory(final String aName, final String aDescription, final boolean isActive) throws Exception {
+        final var requestBody = new CreateCategoryRequest(aName,aDescription,isActive);
+
+        final var aRequest = post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Json.writeValueAsString(requestBody));
+
+        final var actualId = this.mvc.perform(aRequest)
+                .andExpect(status().isCreated())
+                .andReturn()
+                .getResponse().getHeader("Location")
+                .replace("/categories/", "");
+
+        return CategoryID.from(actualId);
     }
 }
