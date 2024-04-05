1338392ff82c98e7e00408aeab66a28660ddbbef
Arthur Saito <arthur_saito1998@hotmail.com>
2024-04-04 23:26:19 -0300

    [test] add a throw exception error test for genre

--- domain/src/test/java/com/pixelflicks/admin/catalogo/domain/genre/GenreTest.java
+++ domain/src/test/java/com/pixelflicks/admin/catalogo/domain/genre/GenreTest.java
@@ -1,12 +1,17 @@
 package com.pixelflicks.admin.catalogo.domain.genre;
 
+import com.pixelflicks.admin.catalogo.domain.exceptions.DomainException;
+import com.pixelflicks.admin.catalogo.domain.validation.handler.ThrowsValidationHandler;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class GenreTest {
 
+    @Test
     public void givenValidParams_whenCallNewGenre_shouldInstantiateAGenre(){
         final var expectedName = "Ação";
         final var expectedIsActive = true;
+        final var expectedCategories = 0;
 
         final var actualGenre = Genre.newGenre(expectedName,expectedIsActive);
 
@@ -19,4 +24,22 @@ public class GenreTest {
         Assertions.assertNotNull(actualGenre.getUpdatedAt());
         Assertions.assertNotNull(actualGenre.getDeletedAt());
     }
+
+    @Test
+    public void givenValidParams_whenCallNewGenreAndValidate_shouldReceiveAError(){
+        final String expectedName = null;
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 0;
+        final var expectedErrorMessage = "Name should not be null";
+
+        final var actualGenre = Genre.newGenre(expectedName,expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () ->{
+            actualGenre.validate(new ThrowsValidationHandler());
+        });
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
 }
