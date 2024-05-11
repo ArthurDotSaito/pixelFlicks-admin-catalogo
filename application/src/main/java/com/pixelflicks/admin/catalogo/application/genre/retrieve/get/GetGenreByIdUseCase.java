6327958c832303dec57ad1c97669ba7b545189ce
Arthur Saito <arthur_saito1998@hotmail.com>
2024-05-10 23:16:10 -0300

    [feat] implementing getGenreById useCase

---
+++ application/src/main/java/com/pixelflicks/admin/catalogo/application/genre/retrieve/get/GetGenreByIdUseCase.java
@@ -0,0 +1,6 @@
+package com.pixelflicks.admin.catalogo.application.genre.retrieve.get;
+
+import com.pixelflicks.admin.catalogo.application.UseCase;
+
+public abstract class GetGenreByIdUseCase extends UseCase<String, GenreOutput> {
+}
