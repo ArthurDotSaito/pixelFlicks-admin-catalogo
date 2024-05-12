89b2cc7f7bc45ab2026eb459f00d9ca96512f6a0
Arthur Saito <arthur_saito1998@hotmail.com>
2024-05-11 21:08:26 -0300

    [test] I should be able to list a genre With categories and filters

---
+++ application/src/test/java/com/pixelflicks/admin/catalogo/application/genre/retrieve/list/ListGenreUseCaseTest.java
@@ -0,0 +1,62 @@
+package com.pixelflicks.admin.catalogo.application.genre.retrieve.list;
+
+import com.pixelflicks.admin.catalogo.application.UseCaseTest;
+import com.pixelflicks.admin.catalogo.domain.genre.Genre;
+import com.pixelflicks.admin.catalogo.domain.genre.GenreGateway;
+import com.pixelflicks.admin.catalogo.domain.pagination.Pagination;
+import com.pixelflicks.admin.catalogo.domain.pagination.SearchQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+
+public class ListGenreUseCaseTest extends UseCaseTest {
+
+    @InjectMocks
+    private DefaultListGenreUseCase useCase;
+
+    @Mock
+    private GenreGateway genreGateway;
+
+    @Override
+    protected List<Object> getMocks() {
+        return List.of(genreGateway);
+    }
+
+    @Test
+    public void givenAValidQuery_whenCallsListGenre_shouldReturnGenres(){
+        //given
+        final var actionGenre = Genre.newGenre("Ação", true);
+        final var adventureGenre = Genre.newGenre("Aventura", true);
+        final var genres = List.of(actionGenre, adventureGenre);
+
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedTerms = "A";
+        final var expectedSort = "createdAt";
+        final var expectedDirection = "asc";
+        final var expectedTotal = 2;
+
+        final var expectedPagination = new Pagination<>(expectedPage,expectedPerPage, expectedTotal, genres);
+
+        when(genreGateway.findAll(any())).thenReturn(expectedPagination);
+
+        //when
+        final var aQuery = new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
+        final var actualOutput = useCase.execute(aQuery);
+
+        //then
+        Assertions.assertEquals(expectedPage, actualOutput.currentPage());
+        Assertions.assertEquals(expectedPerPage, actualOutput.perPage());
+        Assertions.assertEquals(expectedTotal, actualOutput.totalItems());
+        Assertions.assertEquals(expectedItems, actualOutput.items());
+
+        verify(genreGateway, times(1)).findAll(aQuery);
+    }
+}
