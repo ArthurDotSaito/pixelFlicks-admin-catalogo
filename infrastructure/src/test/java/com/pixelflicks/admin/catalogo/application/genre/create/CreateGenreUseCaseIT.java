7dc8e84a598acbf34205c973dcaffe1f81e7ddad
Arthur Saito <arthur_saito1998@hotmail.com>
2024-05-21 00:20:05 -0300

    .

---
+++ infrastructure/src/test/java/com/pixelflicks/admin/catalogo/application/genre/create/CreateGenreUseCaseIT.java
@@ -0,0 +1,64 @@
+package com.pixelflicks.admin.catalogo.application.genre.create;
+
+import com.pixelflicks.admin.catalogo.IntegrationTest;
+import com.pixelflicks.admin.catalogo.domain.category.CategoryGateway;
+import com.pixelflicks.admin.catalogo.domain.category.CategoryID;
+import com.pixelflicks.admin.catalogo.domain.genre.GenreGateway;
+import com.pixelflicks.admin.catalogo.infrastructure.genre.persistence.GenreRepository;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@IntegrationTest
+public class CreateGenreUseCaseIT {
+
+    @Autowired
+    private CreateGenreUseCase useCase;
+
+    @SpyBean
+    private CategoryGateway categoryGateway;
+
+    @SpyBean
+    private GenreGateway genreGateway;
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    public void givenAValidCommand_whenCallsCreateGenre_shouldReturnGenreId(){
+        //given
+        final var expectedName = "Ação";
+        final var expectedIsActive = true;
+        final var expectedCategories = List.<CategoryID>of();
+
+        final var aCommand = CreateGenreCommand.with(expectedName,expectedIsActive, asString(expectedCategories));
+        when(genreGateway.create(any()))
+                .thenAnswer(returnsFirstArg());
+        //when
+        final var actualOutput = useCase.execute(aCommand);
+
+        //then
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.id());
+
+        verify(genreGateway, times(1)).create(Mockito.argThat(aGenre ->
+                Objects.equals(expectedName, aGenre.getName())
+                        && Objects.equals(expectedIsActive, aGenre.isActive())
+                        && Objects.equals(expectedCategories, aGenre.getCategories())
+                        && Objects.nonNull(aGenre.getCreatedAt())
+                        && Objects.nonNull(aGenre.getUpdatedAt())
+                        && Objects.isNull(aGenre.getDeletedAt())
+        ));
+    }
+
+    private List<String> asString(final List<CategoryID> categoriesIds){
+        return categoriesIds.stream().map(CategoryID::getValue).toList();
+    }
+}
