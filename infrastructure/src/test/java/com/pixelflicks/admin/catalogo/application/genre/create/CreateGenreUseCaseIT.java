25af5f9fa256c727acd7d0fcc56ba85362ebda96
Arthur Saito <arthur_saito1998@hotmail.com>
2024-05-22 23:53:27 -0300

    [test] add more genre-ti test cases

--- infrastructure/src/test/java/com/pixelflicks/admin/catalogo/application/genre/create/CreateGenreUseCaseIT.java
+++ infrastructure/src/test/java/com/pixelflicks/admin/catalogo/application/genre/create/CreateGenreUseCaseIT.java
@@ -4,6 +4,7 @@ import com.pixelflicks.admin.catalogo.IntegrationTest;
 import com.pixelflicks.admin.catalogo.domain.category.Category;
 import com.pixelflicks.admin.catalogo.domain.category.CategoryGateway;
 import com.pixelflicks.admin.catalogo.domain.category.CategoryID;
+import com.pixelflicks.admin.catalogo.domain.exceptions.NotificationException;
 import com.pixelflicks.admin.catalogo.domain.genre.GenreGateway;
 import com.pixelflicks.admin.catalogo.infrastructure.genre.persistence.GenreRepository;
 import org.junit.jupiter.api.Assertions;
@@ -112,6 +113,85 @@ public class CreateGenreUseCaseIT {
         Assertions.assertEquals(expectedIsActive, actualGenre.isActive());
         Assertions.assertTrue(expectedCategories.size() == actualGenre.getCategoryIDs().size()
                 && expectedCategories.containsAll(actualGenre.getCategoryIDs()));
+    }
+
+    @Test
+    public void givenAInvalidEmptyName_whenCallsCreateGenre_shouldReturnDomainException(){
+        //given
+        final var expectedName = " ";
+        final var expectedIsActive = true;
+        final var expectedCategories = List.<CategoryID>of();
+        final var expectedErrorMessage = "A 'name' should not be empty";
+        final var expectedErrorCount = 1;
+
+        final var aCommand = CreateGenreCommand.with(expectedName,expectedIsActive, asString(expectedCategories));
+
+        //when
+        final var actualException = Assertions.assertThrows(NotificationException.class, () ->{
+            useCase.execute(aCommand);
+        });
+
+        //then
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        verify(categoryGateway, times(0)).existsByIds(any());
+        verify(genreGateway, times(0)).create(any());
+    }
+
+    @Test
+    public void givenAInvalidNullName_whenCallsCreateGenre_shouldReturnDomainException(){
+        //given
+        final String expectedName = null;
+        final var expectedIsActive = true;
+        final var expectedCategories = List.<CategoryID>of();
+        final var expectedErrorMessage = "A 'name' should not be null";
+        final var expectedErrorCount = 1;
+
+
+        final var aCommand = CreateGenreCommand.with(expectedName,expectedIsActive, asString(expectedCategories));
+
+        //when
+        final var actualException = Assertions.assertThrows(NotificationException.class, () ->{
+            useCase.execute(aCommand);
+        });
+
+        //then
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        verify(categoryGateway, times(0)).existsByIds(any());
+        verify(genreGateway, times(0)).create(any());
+    }
+    
+    @Test
+    public void givenAInvalidName_whenCallsCreateGenreAndSomeCategoriesDoesNotExist_shouldReturnDomainException(){
+        //given
+        final var series = categoryGateway.create(Category.newCategory("Séries",null, true));
+
+        final var expectedName = " ";
+        final var expectedIsActive = true;
+        final var filmes = CategoryID.from("123");
+        final var documentarios = CategoryID.from("789");
+        final var expectedCategories = List.of(filmes, series.getId(), documentarios);
+        final var expectedErrorMessageOne = "Some categories could not be found: 123, 789";
+        final var expectedErrorMessageTwo = "A 'name' should not be empty";
+        final var expectedErrorCount = 2;
+
+        final var aCommand = CreateGenreCommand.with(expectedName,expectedIsActive, asString(expectedCategories));
+
+        //when
+        final var actualException = Assertions.assertThrows(NotificationException.class, () ->{
+            useCase.execute(aCommand);
+        });
+
+        //then
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessageOne, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorMessageTwo, actualException.getErrors().get(1).message());
     }
 
     private List<String> asString(final List<CategoryID> categoriesIds){
