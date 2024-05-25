25bb38f0a4145ce8373536b1e2be732e0b4c7e76
Arthur Saito <arthur_saito1998@hotmail.com>
2024-05-25 16:15:35 -0300

    [test] genreAPI test: return a notification when invalid name

--- infrastructure/src/test/java/com/pixelflicks/admin/catalogo/infrastructure/api/GenreApiTest.java
+++ infrastructure/src/test/java/com/pixelflicks/admin/catalogo/infrastructure/api/GenreApiTest.java
@@ -4,14 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pixelflicks.admin.catalogo.ControllerTest;
 import com.pixelflicks.admin.catalogo.application.genre.create.CreateGenreOutput;
 import com.pixelflicks.admin.catalogo.application.genre.create.CreateGenreUseCase;
+import com.pixelflicks.admin.catalogo.domain.exceptions.NotificationException;
+import com.pixelflicks.admin.catalogo.domain.validation.handler.Notification;
 import com.pixelflicks.admin.catalogo.infrastructure.genre.models.CreateGenreRequest;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.testcontainers.shaded.org.hamcrest.Matchers;
 
 import java.util.List;
 import java.util.Objects;
@@ -20,7 +20,7 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.testcontainers.shaded.org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 
 @ControllerTest(controllers = GenreAPI.class)
 public class GenreApiTest {
@@ -64,5 +64,40 @@ public class GenreApiTest {
                 && Objects.equals(expectedCategories, cmd.categories())
                 && Objects.equals(expectedIsActive, cmd.isActive())
                 ));
+    }
+
+    @Test
+    public void givenAInvalidName_whenCallsCreateGenre_shouldReturnNotification() throws Exception{
+        //given
+        final String expectedName = null;
+        final var expectedCategories = List.of("123","456");
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var aCommand = new CreateGenreRequest(expectedName, expectedCategories,expectedIsActive);
+
+        when(createGenreUseCase.execute(any()))
+                .thenThrow(new NotificationException("Error", Notification.create(new Error(expectedErrorMessage))));
+
+        //when
+        final var aRequest = post("/genres")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(aCommand));
+
+        final var aResponse = this.mvc.perform(aRequest).andDo(print());
+
+        //then
+        aResponse.andExpect(status().isUnprocessableEntity())
+                .andExpect(header().doesNotExist("Location"))
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.errors").isArray())
+                .andExpect(jsonPath("$.errors", hasSize(1)))
+                .andExpect(jsonPath("$.errors[0]", equalTo(expectedErrorMessage)));
+
+        verify(createGenreUseCase).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedCategories, cmd.categories())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+        ));
     }
 }
