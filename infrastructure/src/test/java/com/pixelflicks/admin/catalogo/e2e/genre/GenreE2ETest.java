4fae97965e2b05e9f6a60e11ed58417ef158e185
Arthur Saito <arthur_saito1998@hotmail.com>
2024-06-01 17:54:23 -0300

    [test] e2e test genreCreate without categories

--- infrastructure/src/test/java/com/pixelflicks/admin/catalogo/e2e/genre/GenreE2ETest.java
+++ infrastructure/src/test/java/com/pixelflicks/admin/catalogo/e2e/genre/GenreE2ETest.java
@@ -1,15 +1,28 @@
 package com.pixelflicks.admin.catalogo.e2e.genre;
 
 import com.pixelflicks.admin.catalogo.E2ETest;
+import com.pixelflicks.admin.catalogo.domain.category.CategoryID;
+import com.pixelflicks.admin.catalogo.domain.genre.GenreID;
+import com.pixelflicks.admin.catalogo.infrastructure.category.models.CreateCategoryRequest;
+import com.pixelflicks.admin.catalogo.infrastructure.configuration.json.Json;
+import com.pixelflicks.admin.catalogo.infrastructure.genre.models.CreateGenreRequest;
 import com.pixelflicks.admin.catalogo.infrastructure.genre.persistence.GenreRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+import java.util.function.Function;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @E2ETest
 @Testcontainers
@@ -35,7 +48,44 @@ public class GenreE2ETest {
     }
 
     @Test
-    public void asACatalogAdminIShouldBeAbleToCreateANewGenreWithValidValues(){
+    public void asACatalogAdminIShouldBeAbleToCreateANewGenreWithValidValues() throws Exception{
+        Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0, genreRepository.count());
 
+        final var expectedName = "Ação";
+        final var expectedIsActive = true;
+        final var expectedCategories = List.<CategoryID>of();
+
+        final var actualId = givenAGenre(expectedName,expectedIsActive,expectedCategories);
+
+        final var actualGenre = genreRepository.findById(actualId.getValue()).get();
+
+        Assertions.assertEquals(expectedName, actualGenre.getName());
+        Assertions.assertEquals(expectedIsActive, actualGenre.isActive());
+        Assertions.assertTrue(expectedCategories.size() == actualGenre.getCategoryIDs().size()
+        && expectedCategories.containsAll(actualGenre.getCategoryIDs()));
+        Assertions.assertNotNull( actualGenre.getCreatedAt());
+        Assertions.assertNotNull(actualGenre.getUpdatedAt());
+        Assertions.assertNull( actualGenre.getDeletedAt());
+    }
+
+    private GenreID givenAGenre(final String aName, final boolean isActive, final List<CategoryID> categories) throws Exception {
+        final var requestBody = new CreateGenreRequest(aName,mapTo(categories, CategoryID::getValue),isActive);
+
+        final var aRequest = post("/genres")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Json.writeValueAsString(requestBody));
+
+        final var actualId = this.mvc.perform(aRequest)
+                .andExpect(status().isCreated())
+                .andReturn()
+                .getResponse().getHeader("Location")
+                .replace("/genres/", "");
+
+        return GenreID.from(actualId);
+    }
+
+    private <A,D> List<D> mapTo(final List<A> actual, final Function<A, D> mapper){
+        return actual.stream().map(mapper).toList();
     }
 }
