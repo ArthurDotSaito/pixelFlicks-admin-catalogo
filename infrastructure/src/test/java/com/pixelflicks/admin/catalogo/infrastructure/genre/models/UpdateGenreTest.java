f79b8bf81d3a13a1eb8504bc0f52ecf337c3c144
Arthur Saito <arthur_saito1998@hotmail.com>
2024-05-31 23:48:24 -0300

    [test] genreListResponse mershall test

--- infrastructure/src/test/java/com/pixelflicks/admin/catalogo/infrastructure/genre/models/UpdateGenreTest.java
+++ infrastructure/src/test/java/com/pixelflicks/admin/catalogo/infrastructure/genre/models/UpdateGenreTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.json.JacksonTester;
 
-import java.time.Instant;
 import java.util.List;
 
 @JacksonTest
